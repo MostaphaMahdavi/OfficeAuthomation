bc6430a8da51385054ac6404b63ffd8e590a48dd
Mostapha Mahdavi <Mostapha.Mahdavi@gmail.com>
2020-08-02 12:24:09 +0430

    first commit

---
+++ OfficeAuthomation.TestPresentation/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace OfficeAuthomation.TestPresentation
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
